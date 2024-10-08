69982a1a0e6ea8d841e811a6db395589848620b0
StormLord <stormslordgames@gmail.com>
2024-10-08 09:03:28 +0200

    Comentario en main

--- src/ejercicio_final/Main.java
+++ src/ejercicio_final/Main.java
@@ -1,5 +1,12 @@
 package ejercicio_final;
 
+/**
+ * Clase que ejecuta el programa mediante la creación de un objeto Menú que se encargará de realizar todas las
+ * operaciones que desea el usuario
+ *
+ * @author Alejandro Campos
+ * @version 06102024A
+ */
 public class Main {
     public static void main(String[] args) {
         new Menu();
